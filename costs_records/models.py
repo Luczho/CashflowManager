eac668e839d866c1939ad74c78506de4b284e3d1
Luczho <luczho.bartek@gmail.com>
2023-12-11 23:12:36 +0100

    refactor: continuing working on models

--- costs_records/models.py
+++ costs_records/models.py
@@ -1,61 +1,87 @@
 from django.db import models
 
 
-class Address(models.Model):
-    street_name = models.CharField(max_length=50)
-    building_number = models.CharField(max_length=20)
-    postal_code = models.IntegerField()
-    city = models.CharField(max_length=30)
-
-
 class Company(models.Model):
     symbol = models.CharField(max_length=10, help_text='symbol of the company')
     name = models.CharField(max_length=100)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE, related_name='company') # address.object.first().company
     vat_eu = models.CharField(max_length=20)
     is_contractor = models.BooleanField()
 
 
+class Address(models.Model):
+    company = models.OneToOneField(Company, on_delete=models.DO_NOTHING)
+    street_name = models.CharField(max_length=50)
+    building_number = models.CharField(max_length=20)
+    postal_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=30)
+
+
 class BankAccount(models.Model):
+    EURO = "EUR"
+    ZLOTY = "PLN"
+    DOLAR = "USD"
+    KORONA = "CZK"
+    LEJ = "RON"
+    FORINT = "HUF"
+    CURRENCY_CHOICES = [
+        (EURO, "Euro"),
+        (ZLOTY, "Zloty"),
+        (DOLAR, "Dolar"),
+        (KORONA, "Korona"),
+        (LEJ, "Lej"),
+        (FORINT, "Forint")
+    ]
+
     company = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
     bank_name = models.CharField(max_length=50)
-    currency = models.CharField(max_length=20) # convert to choice field
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=ZLOTY)
     country_code = models.CharField(max_length=20)
-    account_number = models.CharField()
+    account_number = models.CharField(max_length=30)
 
-
-class Invoice(models.Model):
-    date = models.DateField()
-    number = models.CharField(max_length=50)
-    is_proforma = models.BooleanField(default=False)
-    due_date = models.DateField()
-    currency = models.CharField(max_length=20)
-    gross_amount = models.DecimalField(max_digits=10)
-    net_amount = models.DecimalField(max_digits=10)
-    pln_net_amount = ...
-    pln_gross_amount = ...
-
-
-    # @property
-    # def pln_amount(self):
-    #     return self.gross_amount * ...
-    #     ... # calculate net amount from gross amount and vat rate
-
-
-# x = Invoice.objects.first().pln_amount
-
-# class Item(models.Model):
-#     invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, related_name='items')
 
 class ExchangeRate(models.Model):
     date = models.DateField(auto_now_add=True)
-    currency = models.CharField
+    currency = models.CharField()
+
+
+class Invoice(models.Model):
+    EURO = "EUR"
+    ZLOTY = "PLN"
+    DOLAR = "USD"
+    KORONA = "CZK"
+    CURRENCY_CHOICES = [
+        (EURO, "Euro"),
+        (ZLOTY, "Zloty"),
+        (DOLAR, "Dolar"),
+        (KORONA, "Korona")
+    ]
+
+    date = models.DateField()
+    number = models.CharField(max_length=50)
+    proforma = models.BooleanField(default=False)
+    due_date = models.DateField()
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=ZLOTY)
+    gross_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    net_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    pln_net_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    pln_gross_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    vat_rate = models.IntegerField(max_length=3)
+    file = models.FilePathField()
+    printed = models.BooleanField(default=False)
+    in_optima = models.BooleanField(default=False)
 
 
 class Cost(models.Model):
     uid = models.CharField(max_length=50, help_text='unique id of the cost id_created_date_company_symbol ex. 1_2020-01-01_NUT')
     created_date = models.DateField(auto_now_add=True)
-    customer = models.ForeignKey(Company, on_delete=models.DO_NOTHING, related_name='costs') # cost.customer, customer.costs
+    customer = models.ForeignKey(Company, on_delete=models.DO_NOTHING, related_name='costs') # company.costs.all()
     supplier = models.ForeignKey(Company, on_delete=models.DO_NOTHING, related_name='costs')
     cost_description = models.TextField()
-    invoice = models.ForeignKey(Invoice, null=True, on_delete=models.DO_NOTHING, related_name='costs')
+    invoice = models.OneToOneField(Invoice, null=True, on_delete=models.DO_NOTHING)
+    paid = models.BooleanField(default=False)
+    payment_date = models.DateField()
+    asap = models.BooleanField(default=False)
+    urgent = models.BooleanField(default=False)
+
+
+
