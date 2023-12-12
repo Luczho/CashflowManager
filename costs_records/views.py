087abbce9eabe874916a4b4fc7545bcaea496c13
Luczho <luczho.bartek@gmail.com>
2023-12-13 00:14:58 +0100

    feat: create new class-based view

--- costs_records/views.py
+++ costs_records/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Cost
 
 
 def about(request):
-    return render(request, 'costs_records/upgrade-to-pro.html', {'title': 'About'})
+    return render(request, 'costs_records/bootstrap-tables.html', {'title': 'About'})
 
 
+class CostListView(ListView):
+    model = Cost
+    template_name = 'costs_records/costs-table.html'
+    context_object_name = 'costs'
 
-# Create your views here.
+    fields_names = Cost._meta.get_fields()
