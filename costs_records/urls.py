2e7bccacdf882fc4ea2939e5d4290aa22153f872
Luczho <luczho.bartek@gmail.com>
2023-12-13 00:12:08 +0100

    feat: add path to new view

--- costs_records/urls.py
+++ costs_records/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import CostListView
 
 urlpatterns = [
-    path('', views.about, name='cost-about')
+    path('', views.about, name='cost-about'),
+    path('costs/', CostListView.as_view(), name='costs-list')
 ]
