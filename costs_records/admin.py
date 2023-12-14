d71be6f9703316fe021e3d9495f766b7c9a38fdc
Luczho <luczho.bartek@gmail.com>
2023-12-14 23:04:18 +0100

    feat: add model to admin panel

--- costs_records/admin.py
+++ costs_records/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Cost, Company, Invoice, ExchangeRate, BankAccount, Address
+
+admin.site.register(Cost)
+admin.site.register(Company)
+admin.site.register(Invoice)
+admin.site.register(ExchangeRate)
+admin.site.register(BankAccount)
+admin.site.register(Address)
 
 # Register your models here.
