0a12fbb5a010bf0c47345df8875f739e9cd5871b
Luczho <luczho.bartek@gmail.com>
2023-12-20 00:30:54 +0100

    feat: add new AddCompanyView and add multiform handling in AddCostView

--- costs_records/views.py
+++ costs_records/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Cost
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView, TemplateView, FormView
+from .models import Cost, Company, Address
+from .forms import CostForm, InvoiceForm
+from django.urls import reverse_lazy
 
 
 def about(request):
-    return render(request, 'costs_records/bootstrap-tables.html', {'title': 'About'})
+    return render(request, 'costs_records/buttons.html', {'title': 'About'})
 
 
 class CostListView(ListView):
@@ -16,3 +18,38 @@ class CostListView(ListView):
         context = super().get_context_data(**kwargs)
         context['cost_fields'] = Cost._meta.get_fields()
         return context
+
+
+class AddCostView(TemplateView):
+    model = Cost
+    template_name = 'costs_records/add-cost-form.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['forms'] = [CostForm, InvoiceForm]
+    #
+    #     return context
+
+    def get(self, request, *args, **kwargs):
+        cost_form = CostForm(prefix='cost')
+        invoice_form = InvoiceForm(prefix='invoice')
+        return render(request, 'costs_records/add-cost-form.html', {'cost_form': cost_form, 'invoice_form': invoice_form})
+
+        # return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        cost_form = CostForm(self.request.POST, self.request.FILES, prefix='cost')
+        invoice_form = InvoiceForm(self.request.POST, self.request.FILES, prefix='invoice')
+        if all([cost_form.is_valid(), invoice_form.is_valid()]):
+            form = cost_form.save()
+            invoice = invoice_form.save(commit=False)
+            invoice.form = form
+            invoice.save()
+            return redirect('costs-list')
+
+        return super().post(request, *args, **kwargs)
+
+
+class AddCompanyView(TemplateView):
+    model = Company, Address
+    template_name = 'costs_records/add-company-form.html'
