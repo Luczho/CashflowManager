135c7a2d0e303044e2293d88385e7cd7d3a3f1d3
Luczho <luczho.bartek@gmail.com>
2023-12-20 00:25:27 +0100

    refactor: add crispy_forms and crispy_bootstrap5

--- CashflowManager/settings.py
+++ CashflowManager/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'costs_records',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +129,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
