6fbf1ab7b45905969ac4258d0bd2330dabdaaec9
Luczho <luczho.bartek@gmail.com>
2023-12-20 00:32:26 +0100

    feat: add forms to all models

---
+++ costs_records/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from django.contrib.admin.widgets import AdminDateWidget
+from django.forms.fields import DateField
+from .models import Company, Address, BankAccount, Invoice, ExchangeRate, Cost
+
+
+class CostForm(forms.ModelForm):
+
+    class Meta:
+        model = Cost
+        fields = ['customer', 'supplier', 'cost_description', 'payment_date']
+
+    def __init__(self, *args, **kwargs):
+        super(CostForm, self).__init__(*args, **kwargs)
+
+        self.fields['payment_date'].required = False
+        self.fields['customer'].queryset = Company.objects.filter(is_contractor=False)
+        self.fields['supplier'].queryset = Company.objects.filter(is_contractor=True)
+
+
+
+class CompanyForm(forms.ModelForm):
+    class Meta:
+        model = Invoice
+        fields = '__all__'
+
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+
+class BankAccountForm(forms.ModelForm):
+    class Meta:
+        model = BankAccount
+        fields = '__all__'
+
+
+class InvoiceForm(forms.ModelForm):
+    class Meta:
+        model = Invoice
+        fields = ['date', 'number', 'proforma', 'due_date', 'currency', 'gross_amount', 'net_amount', 'vat_rate',
+                  'file', 'printed', 'in_optima']
+
+    def __init__(self, *args, **kwargs):
+        super(InvoiceForm, self).__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].required = False
+
+        # self.fields['date'].required = False
+
+            #
+            # any(for field in forms.fields if fields is not None):
+            #     ... form.save()
