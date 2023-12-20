29835f0ae393e1de38abdfdc4417db0dad25d9b6
Luczho <luczho.bartek@gmail.com>
2023-12-20 22:56:04 +0100

    fix: corrected model in ComapnyForm

--- costs_records/forms.py
+++ costs_records/forms.py
@@ -6,6 +6,8 @@ from .models import Company, Address, BankAccount, Invoice, ExchangeRate, Cost
 
 class CostForm(forms.ModelForm):
 
+    payment_date = forms.DateField(widget=forms.SelectDateWidget)
+
     class Meta:
         model = Cost
         fields = ['customer', 'supplier', 'cost_description', 'payment_date']
@@ -18,10 +20,9 @@ class CostForm(forms.ModelForm):
         self.fields['supplier'].queryset = Company.objects.filter(is_contractor=True)
 
 
-
 class CompanyForm(forms.ModelForm):
     class Meta:
-        model = Invoice
+        model = Company
         fields = '__all__'
 
 
@@ -38,6 +39,10 @@ class BankAccountForm(forms.ModelForm):
 
 
 class InvoiceForm(forms.ModelForm):
+
+    date = forms.DateField(widget=forms.SelectDateWidget)
+    due_date = forms.DateField(widget=forms.SelectDateWidget)
+
     class Meta:
         model = Invoice
         fields = ['date', 'number', 'proforma', 'due_date', 'currency', 'gross_amount', 'net_amount', 'vat_rate',
@@ -49,7 +54,6 @@ class InvoiceForm(forms.ModelForm):
         for field in self.fields:
             self.fields[field].required = False
 
-        # self.fields['date'].required = False
 
             #
             # any(for field in forms.fields if fields is not None):
