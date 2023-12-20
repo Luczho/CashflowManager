856a1676b82c6b8edafb8eb2a641928215b431db
Luczho <luczho.bartek@gmail.com>
2023-12-20 23:18:12 +0100

    feat: defined get and post method in  AddCompanyView class

--- costs_records/views.py
+++ costs_records/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, TemplateView, FormView
 from .models import Cost, Company, Address
-from .forms import CostForm, InvoiceForm
+from .forms import CostForm, InvoiceForm, CompanyForm, AddressForm
 from django.urls import reverse_lazy
 
 
 def about(request):
-    return render(request, 'costs_records/buttons.html', {'title': 'About'})
+    return render(request, 'costs_records/bootstrap-tables.html', {'title': 'About'})
 
 
 class CostListView(ListView):
@@ -33,7 +33,8 @@ class AddCostView(TemplateView):
     def get(self, request, *args, **kwargs):
         cost_form = CostForm(prefix='cost')
         invoice_form = InvoiceForm(prefix='invoice')
-        return render(request, 'costs_records/add-cost-form.html', {'cost_form': cost_form, 'invoice_form': invoice_form})
+        return render(self.request, 'costs_records/add-cost-form.html',
+                      {'cost_form': cost_form, 'invoice_form': invoice_form})
 
         # return super().get(request, *args, **kwargs)
 
@@ -53,3 +54,23 @@ class AddCostView(TemplateView):
 class AddCompanyView(TemplateView):
     model = Company, Address
     template_name = 'costs_records/add-company-form.html'
+
+    def get(self, request, *args, **kwargs):
+        company_form = CompanyForm(prefix='company')
+        address_form = AddressForm(prefix='address')
+        return render(self.request, 'costs_records/add-company-form.html', {'company_form': company_form,
+                                                                            'address_form': address_form})
+
+        # return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        company_form = CompanyForm(self.request.POST, self.request.FILES, prefix='company')
+        address_form = AddressForm(self.request.POST, self.request.FILES, prefix='address')
+        if all([company_form.is_valid(), address_form.is_valid()]):
+            form = company_form.save()
+            address = address_form.save(commit=False)
+            address.form = form
+            address.save()
+            return redirect('add-cost')
+
+        return super().post(request, *args, **kwargs)
