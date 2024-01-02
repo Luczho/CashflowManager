d5ee1878d75e15b0c0e7911a2eca4f5501e120a9
Luczho <luczho.bartek@gmail.com>
2024-01-02 23:05:15 +0100

    feat: add add-company view and cost detail view to path

--- costs_records/urls.py
+++ costs_records/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import CostListView, AddCostView, AddCompanyView
+from .views import CostListView, AddCostView, AddCompanyView, CostDetailView
 
 urlpatterns = [
     path('', views.about, name='cost-about'),
     path('costs/', CostListView.as_view(), name='costs-list'),
     path('add/', AddCostView.as_view(), name='add-cost'),
-    path('add/company/', AddCompanyView.as_view(), name='add-company')
+    path('add/company/', AddCompanyView.as_view(), name='add-company'),
+    path('cost/<int:pk>', CostDetailView.as_view(), name='cost-detail'),
 ]
