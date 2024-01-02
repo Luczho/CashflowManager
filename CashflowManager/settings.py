2cb9eb365a9fe083f702690adc495f34b4007b6b
Luczho <luczho.bartek@gmail.com>
2024-01-02 22:49:13 +0100

    add jquery and datepicker to installed apps

--- CashflowManager/settings.py
+++ CashflowManager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'costs_records',
     'crispy_forms',
     'crispy_bootstrap5',
+    'bootstrap_datepicker_plus',
+    'jquery',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,6 +126,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
