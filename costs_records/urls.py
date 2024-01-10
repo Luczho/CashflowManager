bf44b64d4204b47cf3f59e2940bcab723e2bf710
Luczho <luczho.bartek@gmail.com>
2024-01-10 22:45:26 +0100

    feat: add urls paths to new views

--- costs_records/urls.py
+++ costs_records/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
-from .views import CostListView, AddCostView, AddCompanyView, CostDetailView
+from .views import CostListView, AddCostView, AddCompanyView, CostDetailView, UpdateCostView, AsapCostListView, UrgentCostListView
 
 urlpatterns = [
     path('', views.about, name='cost-about'),
     path('costs/', CostListView.as_view(), name='costs-list'),
-    path('add/', AddCostView.as_view(), name='add-cost'),
+    path('costs/asap/', AsapCostListView.as_view(), name='costs-list-asap'),
+    path('costs/urgent/', UrgentCostListView.as_view(), name='costs-list-urgent'),
+    path('add/cost/', AddCostView.as_view(), name='add-cost'),
+    path('update/cost/<int:pk>/', UpdateCostView.as_view(), name='update-cost'),
     path('add/company/', AddCompanyView.as_view(), name='add-company'),
-    path('cost/<int:pk>', CostDetailView.as_view(), name='cost-detail'),
+    path('cost/<int:pk>/', CostDetailView.as_view(), name='cost-detail'),
 ]
