37a2a6e8179ab7dc8350d9bcfdb223911d42372c
Luczho <luczho.bartek@gmail.com>
2024-01-24 14:41:42 +0100

    add django_filter to installed apps

--- CashflowManager/settings.py
+++ CashflowManager/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'bootstrap4',
+    'django_filters',
     'bootstrap_datepicker_plus',
     'jquery',
     'django.contrib.admin',
