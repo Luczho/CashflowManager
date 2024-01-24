b23fbf5c27113c895877394fe3fd45f48138c05b
Luczho <luczho.bartek@gmail.com>
2024-01-24 14:55:07 +0100

    refactor: added path to new views

--- costs_records/urls.py
+++ costs_records/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-from .views import CostListView, AddCostView, AddCompanyView, CostDetailView, UpdateCostView, AsapCostListView, UrgentCostListView
+from .models import Cost
+from django_filters.views import object_filter
+from .views import CostListView, AddCostView, AddCompanyView, CostDetailView, UpdateCostView, AsapCostListView,\
+    UrgentCostListView, InvoiceListView, AsapUpdateCostView, CostFilterView
 
 urlpatterns = [
     path('', views.about, name='cost-about'),
@@ -11,4 +14,8 @@ urlpatterns = [
     path('update/cost/<int:pk>/', UpdateCostView.as_view(), name='update-cost'),
     path('add/company/', AddCompanyView.as_view(), name='add-company'),
     path('cost/<int:pk>/', CostDetailView.as_view(), name='cost-detail'),
+    path('invoices/', InvoiceListView.as_view(), name='invoice-list'),
+    path('update/cost/asap/<int:pk>', AsapUpdateCostView.as_view(), name='update-cost-asap'),
+    path('costs/filter', CostFilterView.as_view(), name="costs-list-filter")
+
 ]
