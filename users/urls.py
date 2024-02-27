4fa03f4ccd49bdf677024d1925c1f0870d440102
Luczho <luczho.bartek@gmail.com>
2024-02-28 00:55:23 +0100

    feat: added paths to views for handle user authentication

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
-from .views import SignUpView
+from .views import SignUpView, MyLoginView
 
 urlpatterns = [
     path('register/', SignUpView.as_view(), name='register'),
+    path('login/', MyLoginView.as_view(), name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
